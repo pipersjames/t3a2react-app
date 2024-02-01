4977ccd51fa2c91596725d8c7fd304343030f29b
james <131738190+pipersjames@users.noreply.github.com>
2024-02-01 22:46:33 +1000

    style: aligned elements
    set out basic layout of elements

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,13 +2,28 @@
 export default function LoginForm() {
     
     return (
-        <div className="loginForm">
+        <div className="loginFormContainer">
             <h2>Stream-Lined</h2>
             <h1>Login To Your Account</h1>
-            <form autoComplete="on">
-                <input type="text" name="emailInput" id="emailInput" value="Email"/>
-                <input type="text" name="passwordInput" id="passwordInput" value="Password"/>
-                <input type="submit" value="submit"/>
+            <form autoComplete="on" className="loginForm">
+                <input type="text" 
+                name="emailInput" 
+                id="emailInput" 
+                value="Email" 
+                className="loginInputs"
+                />
+                <input 
+                type="text" 
+                name="passwordInput" 
+                id="passwordInput" 
+                value="Password" 
+                className="loginInputs"
+                />
+                <input 
+                type="submit" 
+                value="submit"
+                className="loginButton"
+                />
             </form>
             <a href="url">Forgot your password?</a> {/*url link to account recovery page*/ }
         </div>
