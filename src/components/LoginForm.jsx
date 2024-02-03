a917b15dc17b4b20b0e0465a5ffe6783a11cf81e
james <131738190+pipersjames@users.noreply.github.com>
2024-02-04 00:51:44 +1000

    cookie package and login logic
    installed package for handling cookies securely and added some logic for form submission that has been commented out pending the URL for the Server

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,23 +1,59 @@
+import { useState } from "react"
+import Cookies from 'js-cookie'
+import { useNavigate} from 'react-router-dom'
 
 export default function LoginForm() {
     
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit  = async (event) => {
+        event.preventDefault()
+
+        navigate('/')
+    
+    //     try {
+    //         const response = await fetch("apiEndPoint/login")
+
+    //         if (response.ok) {
+    //             const data = await response.json()
+    //             const token = data.token
+
+    //             Cookies.set('jwt', token, { secure: true, sameSite: 'Strict', expires: 7 })
+
+    //             history.push('/')
+
+    //         } else {
+    //             console.error('Authentication failed')
+    //         } 
+
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+     }
+
+
     return (
         <div className="loginFormContainer">
             <h2>Stream-Lined</h2>
             <h1>Login To Your Account</h1>
-            <form autoComplete="on" className="loginForm">
+            <form autoComplete="on" className="loginForm" onSubmit={handleSubmit}>
                 <input type="text" 
                 name="emailInput" 
                 id="emailInput" 
-                value="Email" 
+                value={email} 
                 className="loginInputs"
+                onChange={(e) => setEmail(e.target.value)}
                 />
                 <input 
                 type="text" 
                 name="passwordInput" 
                 id="passwordInput" 
-                value="Password" 
+                value={password} 
                 className="loginInputs"
+                onChange={(e) => setPassword(e.target.value)}
                 />
                 <input 
                 type="submit" 
