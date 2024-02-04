4b5c7e3f58698ea2197c2f267afa01376d242c7d
james <131738190+pipersjames@users.noreply.github.com>
2024-02-04 16:10:15 +1000

    comment code for eslint

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import Cookies from 'js-cookie'
+//import Cookies from 'js-cookie'
 import { useNavigate} from 'react-router-dom'
 
 export default function LoginForm() {
