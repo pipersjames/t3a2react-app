daf96e8b580dc6b0794826c00770c47589e179fc
JitCHEO <adamcjk@gmail.com>
2024-02-04 20:46:45 +1000

    work on a responsive screen
    sort the much details later on

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav>
-            <div className = "navbar-menu">
-                <ul>
-                    <li>
-                        <NavLink to='/Home' activeClassName="active">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/Form Builder' activeClassName="active">Form Builder</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/Forms' activeClassName="active">Forms</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/Actions' activeClassName="active">Actions</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/Logout' activeClassName="active">Logout</NavLink>
-                    </li>
-                </ul>
+            <div className="navbar-menu">
+                <button className="hamburger-icon" onClick={() => setIsOpen(!isOpen)}>☰</button>
+                <div className={`navbar-items ${isOpen ? 'open' : ''}`}>
+                    <ul>
+                        <li>
+                            <NavLink to='/' activeClassName="active">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/FormBuilder' activeClassName="active">Form Builder</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/Forms' activeClassName="active">Forms</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/Actions' activeClassName="active">Actions</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/Logout' activeClassName="active">Logout</NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
 }
+
