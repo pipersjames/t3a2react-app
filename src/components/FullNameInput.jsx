f7d08b9c0ab0d9662f83502ee46d7cb38ce987b1
JitCHEO <adamcjk@gmail.com>
2024-02-08 21:33:05 +1000

    drag and drop

--- src/components/FullNameInput.jsx
+++ src/components/FullNameInput.jsx
@@ -1,22 +1,3 @@
-// import React from 'react';
-
-// const FullNameInput = ({ fullName, onChange }) => {
-//   return (
-//     <div className="form-group">
-//       <label htmlFor="fullName">Full Name:</label>
-//       <input
-//         type="text"
-//         className="form-control"
-//         id="fullName"   
-//         value={fullName}
-//         onChange={onChange}
-//       />
-//     </div>
-//   );
-// };
-
-// export default FullNameInput;
-
 import React from 'react';
 
 const FullNameInput = ({ value, onChange }) => {
