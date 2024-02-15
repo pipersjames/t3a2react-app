807917540f55d01708ff7e3af21da9325ccc8166
JitCHEO <adamcjk@gmail.com>
2024-02-15 21:14:22 +1000

    sorted fileupload error

--- src/pages/FormBuilder.jsx
+++ src/pages/FormBuilder.jsx
@@ -188,8 +188,7 @@ const FormBuilder = () => {
               {formComponents.map((component, index) => (
                 <div key={index} className="col-md-6 mb-3">
                   {/* Render the component */}
-                  {React.createElement(component.type, { key: component.key, onEmailAdded: handleAddComponent, onFullNameAdded: handleAddComponent })}
-                  
+                  {React.createElement(component.type, { key: component.key, onEmailAdded: handleAddComponent, onFullNameAdded: handleAddComponent, onAddComponent: handleAddComponent })}
 
                   {/* Render delete button for each component */}
                   <button className="btn btn-sm btn-primary mt-1" onClick={() => handleDeleteComponent(index)}>Delete</button>
