88cdc87868dedad6d0392981ed27288d8a7f314f
JitCHEO <adamcjk@gmail.com>
2024-02-19 20:33:37 +1000

    able to sent and fetch from the backend

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Table } from "antd"; // Import Table component from Ant Design
-import LongQA from "../components/formComponents/LongQA";
-import ShortQA from "../components/formComponents/ShortQA";
-import EmailInput from "../components/formComponents/EmailInput";
-import FullNameInput from "../components/formComponents/FullNameInput";
+import { Table } from "antd";
 import { ApiContext } from "../contexts/ApiProvider";
 
-export default function FormsPage() {
-  const [forms, setForms] = useState([]);
-  const { apiUrl } = useContext(ApiContext);
+const FormPage = () => {
+  const [formNames, setFormNames] = useState([]);
+  //Context
+  const { apiUrl } = useContext(ApiContext)
 
   useEffect(() => {
-    const fetchFormData = async () => {
+    const fetchFormNames = async () => {
+
       try {
+        // this apiUrl going through Heroku
         const response = await fetch(`${apiUrl}/formTemplates/forms`, {
           method: 'GET',
           headers: {
@@ -23,45 +22,30 @@ export default function FormsPage() {
           throw new Error('Failed to fetch form data');
         }
         const data = await response.json();
-        setForms(data.result);
+        setFormNames(data.forms);
       } catch (error) {
         console.error("Error fetching form data:", error);
       }
     };
     
-    fetchFormData();
+    fetchFormNames();
   }, [apiUrl]);
 
-  // Define columns for the Ant Design table
   const columns = [
     {
-      title: 'Form Name',
-      dataIndex: 'name',
-      key: 'name',
-    }
+      title: "Form Name",
+      dataIndex: "formName",
+      key: "formName",
+    },
+    // Add more columns as needed
   ];
 
   return (
     <div>
-      <h1>Rendering Tests</h1>
-      {/* Render table with form names */}
-      <Table dataSource={forms} columns={columns} pagination={false} />
-      
-      {/* Render form components */}
-      {forms.map((form) => {
-        switch (form.type) {
-          case "longQA":
-            return <LongQA key={form._id} formData={form} />;
-          case "shortQA":
-            return <ShortQA key={form._id} formData={form} />;
-          case "emailInput":
-            return <EmailInput key={form._id} formData={form} />;
-          case "fullNameInput":
-            return <FullNameInput key={form._id} formData={form} />;
-          default:
-            return null;
-        }
-      })}
+      <h1>Form Page</h1>
+      <Table dataSource={formNames} columns={columns} />
     </div>
   );
-}
+};
+
+export default FormPage;
